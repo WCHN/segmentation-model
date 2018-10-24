678b0d10cadae5e979e3aea9e149572c24d94827
Mikael Brudfors <brudfors@gmail.com>
2018-10-24 11:00:20 +0100

    Fix(update_GaussPrior): Now all CT populations are updated

--- code/update_GaussPrior.m
+++ code/update_GaussPrior.m
@@ -16,7 +16,8 @@ P           = numel(populations);
 
 clusters = {};  
 cnt      = 1;
-    
+has_ct   = false;
+
 for p=1:P % Iterate over populations
     population0 = populations{p}.name;
     modality    = populations{p}.type;
@@ -25,6 +26,8 @@ for p=1:P % Iterate over populations
         % Skip non-CT data
         continue
     end
+    
+    has_ct = true;
     
     pr = GaussPrior(population0);
     
@@ -48,17 +51,29 @@ for p=1:P % Iterate over populations
     end
 end
 
-[pr(1:4),extras] = spm_gmm_lib('UpdateHyperPars',clusters,pr(1:4), ...
-                               'constrained',constrained,'figname','CT','verbose',verbose,'lkp',lkp);    
+if has_ct
+    [pr(1:4),extras] = spm_gmm_lib('UpdateHyperPars',clusters,pr(1:4), ...
+                                   'constrained',constrained,'figname','CT','verbose',verbose,'lkp',lkp);    
 
 
-lb_pr                  = pr{6};                                        
-lb_pr.KL_qVpV(end + 1) = extras.lb;  % KL(q(V0) || p(V0))
-lb_pr.ElnDetV          = extras.ldW; % E[ln|V0|]    
-pr{6}                  = lb_pr;    
+    lb_pr                  = pr{6};                                        
+    lb_pr.KL_qVpV(end + 1) = extras.lb;  % KL(q(V0) || p(V0))
+    lb_pr.ElnDetV          = extras.ldW; % E[ln|V0|]    
+    pr{6}                  = lb_pr;    
 
-GaussPrior(population0) = pr;
-    
+    for p=1:P % Iterate over populations
+        population0 = populations{p}.name;
+        modality    = populations{p}.type;
+
+        if ~strcmpi(modality,'ct')
+            % Skip non-CT data
+            continue
+        end
+
+        GaussPrior(population0) = pr;
+    end
+end
+
 %--------------------------------------------------------------------------
 % Then we update for MR data, where all populations have their individual
 % GaussPriors
