d8000b3dfadb3d6f7021c6466de8052cc337bb54
Mikael Brudfors <brudfors@gmail.com>
2018-11-28 16:48:22 +0000

    Defaults(seg_globals): Use circulant bc for spm_field

--- code/set_globals.m
+++ code/set_globals.m
@@ -4,7 +4,9 @@ function set_globals
 rng('default');
 rng(1);
 
-% Set boundary conditions (BOUND_CIRCULANT 0, BOUND_NEUMANN 1)
+% Set boundary conditions 
+% BOUND_CIRCULANT 0 - a point that disappears off the right side of field of view, will appear again on the left.
+% BOUND_NEUMANN 1   - gradient is zero at the edge of the field of view
 spm_diffeo('boundary',0); 
-spm_field('boundary',1);
+spm_field('boundary',0);
 %==========================================================================
