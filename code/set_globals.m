776f489ea528625cb89d442dd70ca3582f33037c
Mikael Brudfors <brudfors@gmail.com>
2018-12-06 14:44:29 +0000

    Default(set_globals): spm_field('boundary',1)

--- code/set_globals.m
+++ code/set_globals.m
@@ -8,5 +8,5 @@ rng(1);
 % BOUND_CIRCULANT 0 - a point that disappears off the right side of field of view, will appear again on the left.
 % BOUND_NEUMANN 1   - gradient is zero at the edge of the field of view
 spm_diffeo('boundary',0); 
-spm_field('boundary',0);
+spm_field('boundary',1);
 %==========================================================================
