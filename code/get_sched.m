129d107bb39e96e0a7eda96a4a9ef34c234cec0c
Mikael Brudfors <brudfors@gmail.com>
2019-01-27 12:38:17 +0000

    Default(get_sched)

--- code/get_sched.m
+++ code/get_sched.m
@@ -3,8 +3,8 @@ def = spm_shoot_defaults;
 
 if opt.template.do
     sched.gmm = opt.gmm.niter;
-    sched.reg = def.sched;
-    sched.eul = def.eul_its(3:end);
+    sched.reg = [def.sched];
+    sched.eul = Inf;
 else    
     sched.gmm = opt.gmm.niter;       
     sched.reg = def.sched;      
