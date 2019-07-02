ac2417b4aa5d59745c8d489b3bc7fcc3c794e80c
Mikael Brudfors <brudfors@gmail.com>
2019-07-02 14:41:40 +0100

    Defaults: no template decreaing blurring

--- code/get_sched.m
+++ code/get_sched.m
@@ -3,15 +3,15 @@ def = spm_shoot_defaults;
 
 if opt.template.do
     sched.gmm = opt.gmm.niter;
-    sched.reg = def.sched;
+    sched.reg = def.sched(2:end);
     sched.eul = Inf;
 else    
     sched.gmm = opt.gmm.niter;       
-    sched.reg = def.sched;      
+    sched.reg = def.sched(2:end);      
     sched.eul = Inf;
 end
 
-sched.a = [64 def.sched(1:end)];
+sched.a = 1;%[def.sched(2:end)];
 
 sched.labels = opt.gmm.labels.S;
 %==========================================================================
